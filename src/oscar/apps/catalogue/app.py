c6e7cd96531e9fa0a158a47e4c6b737d2b8bdf9d
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-09-14 17:07:30 +0200

    Move ProductDetailview

--- src/oscar/apps/catalogue/app.py
+++ src/oscar/apps/catalogue/app.py
@@ -1,18 +1,12 @@
-from django.conf.urls import url
-
 from oscar.core.application import Application
-from oscar.core.loading import get_class
 
 
 class CatalogueApplication(Application):
     name = 'catalogue'
-    detail_view = get_class('catalogue.views', 'ProductDetailView')
+
 
     def get_urls(self):
         urlpatterns = super(CatalogueApplication, self).get_urls()
-        urlpatterns += [
-            url(r'^(?P<product_slug>[\w-]*)_(?P<pk>\d+)/$',
-                self.detail_view.as_view(), name='detail')]
         return self.post_process_urls(urlpatterns)
 
 
