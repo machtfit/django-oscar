936714b4eceead31fb657f2d93a291d06373ff06
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-09-25 12:32:11 +0200

    Move PageTitleMixin

--- src/oscar/apps/customer/mixins.py
+++ src/oscar/apps/customer/mixins.py
@@ -10,26 +10,6 @@ User = get_user_model()
 logger = logging.getLogger('oscar.customer')
 
 
-class PageTitleMixin(object):
-    """
-    Passes page_title and active_tab into context, which makes it quite useful
-    for the accounts views.
-
-    Dynamic page titles are possible by overriding get_page_title.
-    """
-    page_title = None
-    active_tab = None
-
-    # Use a method that can be overridden and customised
-    def get_page_title(self):
-        return self.page_title
-
-    def get_context_data(self, **kwargs):
-        ctx = super(PageTitleMixin, self).get_context_data(**kwargs)
-        ctx.setdefault('page_title', self.get_page_title())
-        ctx.setdefault('active_tab', self.active_tab)
-        return ctx
-
 
 class RegisterUserMixin(object):
     def register_user(self, form):
