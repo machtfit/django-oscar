a29953182c201e73125c882a78d0265eeb85b6d0
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-10-19 17:30:53 +0200

    Move Line

--- src/oscar/apps/order/models.py
+++ src/oscar/apps/order/models.py
@@ -1,5 +1,4 @@
 from oscar.core.loading import is_model_registered
-from oscar.apps.order.abstract_models import *  # noqa
 from oscar.apps.address.abstract_models import (AbstractShippingAddress,
                                                 AbstractBillingAddress)
 
@@ -18,10 +17,3 @@ if not is_model_registered('order', 'BillingAddress'):
         pass
 
     __all__.append('BillingAddress')
-
-
-if not is_model_registered('order', 'Line'):
-    class Line(AbstractLine):
-        pass
-
-    __all__.append('Line')
