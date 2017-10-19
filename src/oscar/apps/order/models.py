0eb07a8f86a1fe0075714f2bc53a5dfda3c5f727
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-10-19 17:21:21 +0200

    Move ShippingEventQuantity

--- src/oscar/apps/order/models.py
+++ src/oscar/apps/order/models.py
@@ -3,7 +3,7 @@ from oscar.apps.order.abstract_models import *  # noqa
 from oscar.apps.address.abstract_models import (AbstractShippingAddress,
                                                 AbstractBillingAddress)
 
-__all__ = ['PaymentEventQuantity', 'ShippingEventQuantity']
+__all__ = ['PaymentEventQuantity']
 
 
 if not is_model_registered('order', 'ShippingAddress'):
