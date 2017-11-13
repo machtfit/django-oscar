17f3613454a1f22b311ef4cb60b9cff8b1681584
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-11-13 17:24:59 +0100

    Move BulkEditMixin

--- src/oscar/views/generic.py
+++ src/oscar/views/generic.py
@@ -1,75 +1,11 @@
-import json
-
 from django import forms
 from django.core import validators
 from django.core.exceptions import ValidationError
-from django.shortcuts import redirect
-from django.utils.encoding import smart_str
-from django.contrib import messages
-from django.http import HttpResponse
-from django.utils import six
-from django.utils.six.moves import map
 from django.utils.translation import ugettext_lazy as _
-from django.views.generic.base import View
 
 import phonenumbers
 
-from oscar.core.utils import safe_referrer
 from oscar.core.phonenumber import PhoneNumber
-
-
-class BulkEditMixin(object):
-    """
-    Mixin for views that have a bulk editing facility.  This is normally in the
-    form of tabular data where each row has a checkbox.  The UI allows a number
-    of rows to be selected and then some 'action' to be performed on them.
-    """
-    action_param = 'action'
-
-    # Permitted methods that can be used to act on the selected objects
-    actions = None
-    checkbox_object_name = None
-
-    def get_checkbox_object_name(self):
-        if self.checkbox_object_name:
-            return self.checkbox_object_name
-        return smart_str(self.model._meta.object_name.lower())
-
-    def get_error_url(self, request):
-        return safe_referrer(request, '.')
-
-    def get_success_url(self, request):
-        return safe_referrer(request, '.')
-
-    def post(self, request, *args, **kwargs):
-        # Dynamic dispatch pattern - we forward POST requests onto a method
-        # designated by the 'action' parameter.  The action has to be in a
-        # whitelist to avoid security issues.
-        action = request.POST.get(self.action_param, '').lower()
-        if not self.actions or action not in self.actions:
-            messages.error(self.request, _("Invalid action"))
-            return redirect(self.get_error_url(request))
-
-        ids = request.POST.getlist(
-            'selected_%s' % self.get_checkbox_object_name())
-        ids = list(map(int, ids))
-        if not ids:
-            messages.error(
-                self.request,
-                _("You need to select some %ss")
-                % self.model._meta.verbose_name_plural)
-            return redirect(self.get_error_url(request))
-
-        objects = self.get_objects(ids)
-        return getattr(self, action)(request, objects)
-
-    def get_objects(self, ids):
-        object_dict = self.get_object_dict(ids)
-        # Rearrange back into the original order
-        return [object_dict[id] for id in ids]
-
-    def get_object_dict(self, ids):
-        return self.get_queryset().in_bulk(ids)
 
 
 class PhoneNumberMixin(object):
