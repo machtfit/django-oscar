f7ddeb9ed884fb37774d030aeb9d7b8b457099df
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-11-29 12:12:37 +0100

    Move parter config

--- src/oscar/apps/partner/config.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class PartnerConfig(AppConfig):
-    label = 'partner'
-    name = 'oscar.apps.partner'
-    verbose_name = _('Partner')
