1213c5d243de6888d6c2f2997707bbe1b824975e
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-12-12 15:59:30 +0100

    Move redirect_to_referrer, safe_referrer

--- src/oscar/core/utils.py
+++ src/oscar/core/utils.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import  # for logging import below
 import logging
 
-from django.shortcuts import redirect, resolve_url
-from django.utils.http import is_safe_url
 from django.utils.timezone import get_current_timezone, is_naive, make_aware
 from django.conf import settings
 from django.template.defaultfilters import (date as date_filter)
@@ -44,33 +42,6 @@ def format_datetime(dt, format=None):
     return date_filter(localtime, format)
 
 
-def safe_referrer(request, default):
-    """
-    Takes the request and a default URL. Returns HTTP_REFERER if it's safe
-    to use and set, and the default URL otherwise.
-
-    The default URL can be a model with get_absolute_url defined, a urlname
-    or a regular URL
-    """
-    referrer = request.META.get('HTTP_REFERER')
-    if referrer and is_safe_url(referrer, request.get_host()):
-        return referrer
-    if default:
-        # Try to resolve. Can take a model instance, Django URL name or URL.
-        return resolve_url(default)
-    else:
-        # Allow passing in '' and None as default
-        return default
-
-
-def redirect_to_referrer(request, default):
-    """
-    Takes request.META and a default URL to redirect to.
-
-    Returns a HttpResponseRedirect to HTTP_REFERER if it exists and is a safe
-    URL; to the default URL otherwise.
-    """
-    return redirect(safe_referrer(request, default))
 
 
 def get_default_currency():
