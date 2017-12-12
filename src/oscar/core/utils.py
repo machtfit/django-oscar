98502658eac07f7721f5c4ce4860365b1074b49f
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-12-12 16:05:14 +0100

    Delete utils compose

--- src/oscar/core/utils.py
+++ src/oscar/core/utils.py
@@ -3,27 +3,6 @@ from __future__ import absolute_import  # for logging import below
 from django.conf import settings
 
 
-def compose(*functions):
-    """
-    Compose functions
-
-    This is useful for combining decorators.
-    """
-    def _composed(*args):
-        for fn in functions:
-            try:
-                args = fn(*args)
-            except TypeError:
-                # args must be scalar so we don't try to expand it
-                args = fn(args)
-        return args
-    return _composed
-
-
-
-
-
-
 def get_default_currency():
     """
     For use as the default value for currency fields.  Use of this function
