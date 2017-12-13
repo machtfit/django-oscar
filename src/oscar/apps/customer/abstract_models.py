75394b61a200b9695ca735f02949ddce28920f33
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-12-13 12:37:45 +0100

    Move AbstractUser

--- src/oscar/apps/customer/abstract_models.py
+++ src/oscar/apps/customer/abstract_models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth import models as auth_models
-from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
 
 
 class UserManager(auth_models.BaseUserManager):
@@ -31,61 +29,3 @@ class UserManager(auth_models.BaseUserManager):
         u.is_superuser = True
         u.save(using=self._db)
         return u
-
-
-class AbstractUser(auth_models.AbstractBaseUser,
-                   auth_models.PermissionsMixin):
-    """
-    An abstract base user suitable for use in Oscar projects.
-
-    This is basically a copy of the core AbstractUser model but without a
-    username field
-    """
-    email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(
-        _('First name'), max_length=255, blank=True)
-    last_name = models.CharField(
-        _('Last name'), max_length=255, blank=True)
-    is_staff = models.BooleanField(
-        _('Staff status'), default=False,
-        help_text=_('Designates whether the user can log into this admin '
-                    'site.'))
-    is_active = models.BooleanField(
-        _('Active'), default=True,
-        help_text=_('Designates whether this user should be treated as '
-                    'active. Unselect this instead of deleting accounts.'))
-    date_joined = models.DateTimeField(_('date joined'),
-                                       default=timezone.now)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-
-    class Meta:
-        abstract = True
-        verbose_name = _('User')
-        verbose_name_plural = _('Users')
-
-    def get_full_name(self):
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
-
-    def get_short_name(self):
-        return self.first_name
-
-    def handle_order_shipped_to(self, shipping_address):
-        """
-        Put the shipping address in the user's address book, or if it's there
-        already, increase its usage counter to track which addresses are used
-        most often for orders.
-        """
-        try:
-            user_address = self.addresses.get(
-                hash=shipping_address.generate_hash())
-        except self.addresses.model.DoesNotExist:
-            # Create a new user address
-            user_address = self.addresses.model(user=self)
-            shipping_address.populate_alternative_model(user_address)
-
-        user_address.num_orders += 1
-        user_address.save()
