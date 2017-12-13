c12a34da8fd50986547a599b3c4d65b4d36eef34
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-12-13 12:16:12 +0100

    Move get_password_reset_url

--- src/oscar/apps/customer/utils.py
+++ src/oscar/apps/customer/utils.py
@@ -1,20 +1,3 @@
-from django.core.urlresolvers import reverse
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-
-
-def get_password_reset_url(user, token_generator=default_token_generator):
-    """
-    Generate a password-reset URL for a given user
-    """
-    kwargs = {
-        'token': token_generator.make_token(user),
-        'uidb64': urlsafe_base64_encode(force_bytes(user.id)),
-    }
-    return reverse('password-reset-confirm', kwargs=kwargs)
-
-
 def normalise_email(email):
     """
     The local part of an email address is case-sensitive, the domain part
